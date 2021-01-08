c30c98531057a22a3660950f67984c48a2906b4f
Clemens Bruesch <clemensbruesch@gmail.com>
2021-01-08 22:12:07 +0100

    template literals

--- JavaScript/Examples/template-literals.js
+++ JavaScript/Examples/template-literals.js
@@ -33,3 +33,27 @@ plus(3,4);
 // 3 + 4 = 7
 
 // source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+
+
+// CODEWARS
+// Coding in function fiveLine, function accept 1 parameter:s. s is a string.
+
+// Please return a string of 5 lines(newline symbol is \n). The first line has one s; Second line have two s; and so on..Fifth line have five s;
+
+// Note1: The two sides of the parameter s may contain some whitespace, please clear them before using s.
+
+// Note2: Using a string template can make your job easier.
+
+let s1 = " a";
+let s2 = "\txy \n";
+let s3 = "           Ok               ";
+
+const fiveLine = (s) => {
+  let fiveLog = "";
+  for (let i = 1; i <= 5; i++){
+    fiveLog += `${s.trim()}\n` 
+  }
+  return fiveLog;
+}
+
+console.log(fiveLine(s1));
