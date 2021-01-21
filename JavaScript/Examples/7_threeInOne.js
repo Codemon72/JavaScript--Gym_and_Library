22291b8fc95b66d6c0d5e7a5e1b139a18c25287e
Clemens Bruesch <clemensbruesch@gmail.com>
2021-01-22 00:03:13 +0100

    threeInOne

--- JavaScript/Examples/7_threeInOne.js
+++ JavaScript/Examples/7_threeInOne.js
@@ -4,14 +4,25 @@
 
 // Note2: Because this is a beginner Kata, and due to the author's mercy ;-), so you do not have to verify the validity of the parameter, and do not worry about the number of elements of the array is not a multiple of 3.
 
+// const threeInOne = (arr) => {
+//   const copyArr = [...arr];
+//   const newArray = [];
+//   for (let i = 0; i < copyArr.length; i + 3){
+//     const temp = copyArr.splice(0, 3);
+//     newArray.push(temp[0] + temp[1] + temp[2]);
+//   }
+//   return newArray;
+// };
+
+// A bit cleaner and shorter:
+
 const threeInOne = (arr) => {
-  const newArray = [];
-  for (let i = 0; i < arr.length; i + 3){
-    const temp = arr.splice(0, 3);
-    newArray.push(temp[0] + temp[1] + temp[2]);
+  const result = [];
+  for(let i=0; i < arr.length; i += 3) {
+    result.push(arr[i] + arr[i+1] + arr[i+2]);
   }
-  return newArray;
-};
+  return result
+}
 
 console.log(threeInOne([1,2,3])); // should return [6]
 console.log(threeInOne([1,2,3,4,5,6])); // should return [6,15]
