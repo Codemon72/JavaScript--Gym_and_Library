791fb8e15535bf1c9b59ac69c4e019bd7c6986fb
Clemens Bruesch <clemensbruesch@gmail.com>
2021-01-22 23:09:09 +0100

    sortIt reverse(), sort()

--- JavaScript/Examples/7_sortIt.js
+++ JavaScript/Examples/7_sortIt.js
@@ -15,7 +15,10 @@
 
 const sortIt = (arr) => {
   const newArray = [...arr];
-  return newArray;
+  return newArray.sort((a, b) => {return b - a});
+  // make on array for each number
+  // sort them by value and length
+  // 
 
 };
 
