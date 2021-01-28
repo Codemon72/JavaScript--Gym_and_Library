ca37827ee8ce4916f6a88932b78d6fba12116770
Clemens Bruesch <clemensbruesch@gmail.com>
2021-01-28 22:12:22 +0100

    map(), slice()

--- JavaScript/Examples/7_isolateIt.js
+++ JavaScript/Examples/7_isolateIt.js
@@ -11,10 +11,28 @@
 // isolateIt(["abcde","fghij"]) should return ["ab|de","fg|ij"]
 // isolateIt(["1234","56789"]) should return ["12|34","56|89"]
 
+// First attempt:
+// const isolateIt = (arr) => {
+//   return arr.map(string => {
+//     let arrayFromString = string.split('');
+//     // if string.length is even: insert method
+//     if (arrayFromString.length % 2 === 0){
+//       arrayFromString.splice((arrayFromString.length / 2), 0, '|')
+//     }
+//     // if string.length is uneven: replacement method
+//     if (arrayFromString.length % 2 !== 0){
+//       arrayFromString.splice((arrayFromString.length / 2), 1, '|')
+//     }
+//     return arrayFromString.join('');
+//   })
+// };
 
-
-
+// Amazingly more simple:
 const isolateIt = (arr) => {
-  //coding here...
-  
-}
+  return arr.map(s => s.slice(0, s.length/2) + '|' + s.slice(-s.length/2))
+};
+
+
+console.log(isolateIt(["abcd","efgh"])); // expected output: ["ab|cd","ef|gh"]);
+console.log(isolateIt(["abcde","fghij"])); // expected output: ["ab|de","fg|ij"]);
+console.log(isolateIt(["1234","56789"])); // expected output: ["12|34","56|89"]);
