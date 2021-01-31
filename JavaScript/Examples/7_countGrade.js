0ac6b13fd4ba592605900abbce986eb345df492e
Clemens Bruesch <clemensbruesch@gmail.com>
2021-01-31 11:37:19 +0100

    first solution with map()

--- JavaScript/Examples/7_countGrade.js
+++ JavaScript/Examples/7_countGrade.js
@@ -12,8 +12,6 @@
 // Grade X: score=-1(The cheating guy gets a score like that)
 
 
-
-
 const countGrade = (scores) => {
   const gradedScores = {
     S: 0,
@@ -25,15 +23,17 @@ const countGrade = (scores) => {
   }
   scores.map( x => {
     if (x === 100){ gradedScores.S += 1 }
-    if (100 > x >= 90){ gradedScores.A += 1 }
-    else { gradedScores.B += 1 }
+    if (100 > x && x >= 90){ gradedScores.A += 1 }
+    if (90 > x && x >= 80){ gradedScores.B += 1 }
+    if (80 > x && x >= 60){ gradedScores.C += 1 }
+    if (60 > x && x >= 0){ gradedScores.D += 1 }
+    if (x === -1){ gradedScores.X += 1 }
   })
   return gradedScores
 };
 
 
-// console.log(countGrade([50,60,70,80,90,100])); // expected outcome: {S:1, A:1, B:1, C:2, D:1, X:0});
-// console.log(countGrade([65,75,,85,85,95,100,100])); // expected outcome: {S:2, A:1, B:2, C:2, D:0, X:0});
-// console.log(countGrade([-1,-1,-1,-1,-1,-1])); // expected outcome: {S:0, A:0, B:0, C:0, D:0, X:6});
+console.log(countGrade([50,60,70,80,90,100])); // expected outcome: {S:1, A:1, B:1, C:2, D:1, X:0});
+console.log(countGrade([65,75,,85,85,95,100,100])); // expected outcome: {S:2, A:1, B:2, C:2, D:0, X:0});
+console.log(countGrade([-1,-1,-1,-1,-1,-1])); // expected outcome: {S:0, A:0, B:0, C:0, D:0, X:6});
 
-console.log(countGrade([100, 95, 75]));
