b154ce420cf20938bb71e5ea6c75e44d643530a6
Clemens Bruesch <clemensbruesch@gmail.com>
2021-07-13 22:41:26 +0200

    pseudo code

--- Exercises/7_Senior_or_Open.js
+++ Exercises/7_Senior_or_Open.js
@@ -5,18 +5,20 @@
 // Input
 // Input will consist of a list of lists containing two items each. Each list contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
 
-// Note for F#: The input will be of (int list list) which is a List<List>
-
 // Example Input
 // [[18, 20],[45, 2],[61, 12],[37, 6],[21, 21],[78, 9]]
 // Output
-// Output will consist of a list of string values (in Haskell: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+// Output will consist of a list of string values stating whether the respective member is to be placed in the senior or open category.
 
 // Example Output
 // ["Open", "Open", "Senior", "Open", "Open", "Senior"]
 
 const openOrSenior = (data) => {
-  
+  let evaluatedData = [];
+  // for loop or forEach: 
+  // if first number >= 55 && second number > 7 push "Senior"
+  // else push "Open"
+  // return evaluatedData  
 }
 
 
