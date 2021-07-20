faf3d91f90675ecdc64e80e06fb2fb3fd9dc0131
Clemens Bruesch <clemensbruesch@gmail.com>
2021-07-20 23:31:29 +0200

    exercise highestRank setup

---
+++ Exercises/6_highestRank.js
@@ -0,0 +1,20 @@
+// Complete the method which returns the number which is most frequent in the given input array. If there is a tie for most frequent number, return the largest number among them.
+
+// Note: no empty arrays will be given.
+
+// Examples
+// [12, 10, 8, 12, 7, 6, 4, 10, 12]              -->  12
+// [12, 10, 8, 12, 7, 6, 4, 10, 12, 10]          -->  12
+// [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]  -->   3
+
+const arr1 = [12, 10, 8, 12, 7, 6, 4, 10, 12]            
+const arr2 = [12, 10, 8, 12, 7, 6, 4, 10, 12, 10]        
+const arr3 = [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]
+
+const highestRank = (arr) => {
+
+}
+
+console.log(highestRank(arr1)); // --> 12
+console.log(highestRank(arr2)); // --> 12
+console.log(highestRank(arr3)); // --> 3
