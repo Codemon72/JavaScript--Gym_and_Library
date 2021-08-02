5a661d3da7446ac888273551b4a76e5b5dfb6a19
Clemens Bruesch <clemensbruesch@gmail.com>
2021-08-02 23:46:48 +0200

    alphabetWar starred solution

--- Exercises/6_alphabetWar.js
+++ Exercises/6_alphabetWar.js
@@ -24,48 +24,56 @@
 // alphabetWar("zzzzs");    //=> Right side wins!
 // alphabetWar("wwwwwwz");  //=> Left side wins!
 
-const alphabetWar = (fight) => {
-  let leftCounter = 0;
-  let rightCounter = 0;
-  let numbers = fight.split("");
-  numbers.forEach((number) => {
-    switch (number) {
-      case "w":
-        leftCounter += 4;
-        break;
-      case "p":
-        leftCounter += 3;
-        break;
-      case "b":
-        leftCounter += 2;
-        break;
-      case "s":
-        leftCounter += 1;
-        break;
-      case "m":
-        rightCounter += 4;
-        break;
-      case "q":
-        rightCounter += 3;
-        break;
-      case "d":
-        rightCounter += 2;
-        break;
-      case "z":
-        rightCounter += 1;
-        break;
-      default:
-        break;
-    }
-  });
-  if (leftCounter > rightCounter) {
-    return "Left side wins!";
-  } else if (leftCounter < rightCounter) {
-    return "Right side wins!";
-  } else if (leftCounter === rightCounter) {
-    return "Let's fight again!";
-  }
-};
+// first solution:
+// const alphabetWar = (fight) => {
+//   let leftCounter = 0;
+//   let rightCounter = 0;
+//   let numbers = fight.split("");
+//   numbers.forEach((number) => {
+//     switch (number) {
+//       case "w":
+//         leftCounter += 4;
+//         break;
+//       case "p":
+//         leftCounter += 3;
+//         break;
+//       case "b":
+//         leftCounter += 2;
+//         break;
+//       case "s":
+//         leftCounter += 1;
+//         break;
+//       case "m":
+//         rightCounter += 4;
+//         break;
+//       case "q":
+//         rightCounter += 3;
+//         break;
+//       case "d":
+//         rightCounter += 2;
+//         break;
+//       case "z":
+//         rightCounter += 1;
+//         break;
+//       default:
+//         break;
+//     }
+//   });
+//   if (leftCounter > rightCounter) {
+//     return "Left side wins!";
+//   } else if (leftCounter < rightCounter) {
+//     return "Right side wins!";
+//   } else if (leftCounter === rightCounter) {
+//     return "Let's fight again!";
+//   }
+// };
+
+// starred solution:
+function alphabetWar(fight) {
+  let map = { w: -4, p: -3, b: -2, s: -1, m: 4, q: 3, d: 2, z: 1 };
+  let result = fight.split('').reduce((a, b) => a + (map[b] || 0), 0);
+  return result ? (result < 0 ? "Left" : "Right") + " side wins!" : "Let's fight again!";
+}
 
 console.log(alphabetWar("z")); //=> Right side wins!
 console.log(alphabetWar("zdqmwpbs")); //=> Let's fight again!
