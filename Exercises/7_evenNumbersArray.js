ad52d134bb65364a65405c28c53f8540f2ebb6a1
Clemens Bruesch <clemensbruesch@gmail.com>
2021-08-12 23:29:26 +0200

    exercise evenNumbers: confirmation set up

--- Exercises/7_evenNumbersArray.js
+++ Exercises/7_evenNumbersArray.js
@@ -7,5 +7,13 @@
 // ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
 
 const evenNumbers = (array, number) => {
-
+  return array;
 }
+
+
+console.table(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)) 
+//=> [4, 6, 8]
+console.log(evenNumbers([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2))
+// => [-8, 26]
+console.log(evenNumbers([6, -25, 3, 7, 5, 5, 7, -3, 23], 1))
+// => [6]
