4465adf528cff621545aa6ef6fe4fbd9a619ad07
Clemens Bruesch <clemensbruesch@gmail.com>
2021-08-14 22:43:12 +0200

    evenNumbers exercise solved

--- Exercises/7_evenNumbersArray.js
+++ Exercises/7_evenNumbersArray.js
@@ -7,18 +7,14 @@
 // ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
 
 const evenNumbers = (array, number) => {
-  let evens = [];
-  evens = array.reverse();
-  
-  for (let i = 0; i < number; i++) {
-  }
-  console.log(evens.filter(n => { return n % 2 === 0}))
+  let evens = array.filter(n => { return n % 2 === 0});
+  return evens.splice(-number);
 }
 
-evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)
-//console.table(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)) 
+
+console.table(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)) 
 //=> [4, 6, 8]
-//console.log(evenNumbers([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2))
+console.log(evenNumbers([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2))
 // => [-8, 26]
-//console.log(evenNumbers([6, -25, 3, 7, 5, 5, 7, -3, 23], 1))
+console.log(evenNumbers([6, -25, 3, 7, 5, 5, 7, -3, 23], 1))
 // => [6]
