b00bfe9d725e82599f9d80cc2ca653bca7a39817
Clemens Bruesch <clemensbruesch@gmail.com>
2021-09-05 21:02:22 +0200

    solution in one line

--- Exercises/8_reversedWords.js
+++ Exercises/8_reversedWords.js
@@ -4,16 +4,21 @@
 
 // "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
 
+// first solution:
+// const reverseWords = (str) => {
+//   // turn into array
+//   let tempArray = str.split(' ');
+//   // reverse array
+//   let tempReversed = tempArray.reverse();
+//   // turn back into string
+//   let reversedStr = tempReversed.join(' ')
+//   return reversedStr;
+// }
 
+// in one line:
 const reverseWords = (str) => {
-  // turn into array
-  let tempArray = str.split(' ');
-  // reverse array
-  let tempReversed = tempArray.reverse();
-  // turn back into string
-  let reversedStr = tempReversed.join(' ')
-  return reversedStr;
-}
+  return str.split(' ').reverse().join(' ');
+};
 
 const str = "The greatest victory is that which requires no battle";
 
