fc84165608938a32a6dd50d0ce9b7227c26fea6b
Clemens Bruesch <clemensbruesch@gmail.com>
2021-09-05 20:53:02 +0200

    exercise reverseWords setup

---
+++ Exercises/8_reversedWords.js
@@ -0,0 +1,14 @@
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+// Example:
+
+// "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+
+const reverseWords = (str) => {
+  return str;
+}
+
+const str = "The greatest victory is that which requires no battle";
+
+console.log(reverseWords(str));
