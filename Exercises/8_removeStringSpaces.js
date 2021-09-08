7eb34da7018f423901ae708e18942e670eae4a12
Clemens Bruesch <clemensbruesch@gmail.com>
2021-09-08 22:25:43 +0200

    exercise with regular expression

--- Exercises/8_removeStringSpaces.js
+++ Exercises/8_removeStringSpaces.js
@@ -1,5 +1,14 @@
 // Simple, remove the spaces from the string, then return the resultant string.
 
-const noSpace = (string) => {
-  return string.split(' ').join('');
+// const noSpace = (string) => {
+//   return string.split(' ').join('');
+// }
+
+// solution with regular expression
+const noSpace = (x) =>{
+  return x.replace(/\s/g, '');
 }
+
+const quote = "There is nothing permanent except change"; 
+
+console.log(noSpace(quote)); // "Thereisnothingpermanentexceptchange"
