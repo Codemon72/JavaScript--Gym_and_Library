9b5c2814ced0c52407149bb91227e3d09c8083db
Clemens Bruesch <clemensbruesch@gmail.com>
2021-09-12 20:35:00 +0200

    first solution

--- Exercises/6_whoLikesIt.js
+++ Exercises/6_whoLikesIt.js
@@ -5,11 +5,30 @@
 // Note: For 4 or more names, the number in "and 2 others" simply increases.
 
 const likes = (names) => {
-  // TODO
-}
+  let displayText = '';
+
+  switch (names.length) {
+    case 0:
+      displayText = 'no one likes this';
+      break;
+    case 1:
+      displayText = `${names[0]} likes this`;
+      break;
+    case 2:
+      displayText = `${names[0]} and ${names[1]} like this`;
+      break;
+    case 3:
+      displayText = `${names[0]}, ${names[1]} and ${names[2]} like this`;
+      break;
+    default:
+      displayText = `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+      break;
+  }
+  return displayText;
+};
 
 console.log(likes([])); //                                -->  "no one likes this"
-console.log(likes(["Peter"])); //                         -->  "Peter likes this"
-console.log(likes(["Jacob", "Alex"])); //                 -->  "Jacob and Alex like this"
-console.log(likes(["Max", "John", "Mark"])); //           -->  "Max, John and Mark like this"
-console.log(likes(["Alex", "Jacob", "Mark", "Max"])); //  -->  "Alex, Jacob and 2 others like this"
+console.log(likes(['Peter'])); //                         -->  "Peter likes this"
+console.log(likes(['Jacob', 'Alex'])); //                 -->  "Jacob and Alex like this"
+console.log(likes(['Max', 'John', 'Mark'])); //           -->  "Max, John and Mark like this"
+console.log(likes(['Alex', 'Jacob', 'Mark', 'Max'])); //  -->  "Alex, Jacob and 2 others like this"
