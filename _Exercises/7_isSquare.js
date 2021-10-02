537defaaf7beafe6709c7c93071c14ded0a38834
Clemens Bruesch <clemensbruesch@gmail.com>
2021-10-02 23:50:54 +0200

    exercise setup

---
+++ _Exercises/7_isSquare.js
@@ -0,0 +1,18 @@
+// Given an integral number, determine if it's a square number:
+
+//     In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+// The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+const isSquare = (n) => {
+  return 'blib'
+}
+
+let num = -1  // =>  false
+// num =  0  // =>  true
+// num =  3  // =>  false
+// num =  4  // =>  true
+// num = 25  // =>  true
+// num = 26  // =>  false
+
+console.log(isSquare(num))
