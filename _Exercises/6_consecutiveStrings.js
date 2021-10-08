9f6bd3bf41334214c1e12572db09365ed331afd0
Clemens Bruesch <clemensbruesch@gmail.com>
2021-10-08 23:49:21 +0200

    highest voted solution

--- _Exercises/6_consecutiveStrings.js
+++ _Exercises/6_consecutiveStrings.js
@@ -36,6 +36,20 @@ const longestConsec = (strarr, k) => {
   return longestConcat
 }
 
+// highest voted solution on platform:
+
+// function longestConsec(strarr, k) {
+//   var longest = "";
+//   for(var i=0;k>0 && i<=strarr.length-k;i++){
+//     var tempArray = strarr.slice(i,i+k);
+//     var tempStr = tempArray.join("");
+//     if(tempStr.length > longest.length){
+//       longest = tempStr;
+//     }
+//   }
+//   return longest;
+// }
+
 const strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 3;
 
 console.log(longestConsec(strarr, k));
