46e49f3ba5cfbba6d46cad6bfa6ad62e82a31be1
Clemens Bruesch <clemensbruesch@gmail.com>
2021-10-10 22:54:15 +0200

    exercise arrayDiff:solution with includes()

--- _Exercises/6_arrayDiff.js
+++ _Exercises/6_arrayDiff.js
@@ -9,21 +9,27 @@
 // arrayDiff([1,2,2,2,3],[2]) == [1,3]
 
 // first working solution:
+// const arrayDiff = (a, b) => {
+//   let newArray = [];
+//   for (let i = 0; i < a.length; i++) {
+//     let aNum = a[i];
+//     let control = false;
+//     for (let j = 0; j < b.length; j++) {
+//       if (a[i] == b[j]) {
+//         control = true;
+//       }
+//     }
+//     if (control === false) {
+//       newArray.push(a[i])
+//     }
+//   }
+//   return newArray
+// }
+
+// solution with .includes():
 const arrayDiff = (a, b) => {
-  let newArray = [];
-  for (let i = 0; i < a.length; i++) {
-    let aNum = a[i];
-    let control = false;
-    for (let j = 0; j < b.length; j++) {
-      if (a[i] == b[j]) {
-        control = true;
-      }
-    }
-    if (control === false) {
-      newArray.push(a[i])
-    }
-  }
-  return newArray
+  return a.filter(num => !b.includes(num));
 }
+
 
 console.log(arrayDiff([7, 1, 2, 3, 4, 5, 6], [2, 5]));
